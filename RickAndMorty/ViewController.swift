bc332ad121ff1958cb6b6e94e1418010ee1e65e9
Alpay Calalli <calalli1903@gmail.com>
2025-10-20 10:32:54 +0400

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Alpay Calalli on 20.10.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
